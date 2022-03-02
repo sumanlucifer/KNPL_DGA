aa3355753c44cc4638c8009b5dc1e03d73f05897
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-03-02 19:06:52 +0000

    added deploy.sh file

---
+++ Deploy.sh
@@ -0,0 +1,16 @@
+SPACE="DGA"
+VER=mta_archives/knpl-dga-web_1.0.0.mtar
+
+#Login to CF space
+cf login -a https://api.cf.ap11.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin@2022 -o "Kansai Nerolac Paints Ltd_knpl-dev-projects" -s "${SPACE}"
+
+echo "Building Project"
+mbt build -s '/home/user/projects/knpl-influencer-web'
+
+if [[ $? -eq 0 ]]; then
+    echo "build complete Now deploying"
+    cf deploy $VER --delete-services
+    git reset --hard
+else
+    echo "corrupt MTA file please check."
+fi    
