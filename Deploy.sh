8c70f245fe0a980ea2e8c709425dbb6abc609b49
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-03-02 19:15:46 +0000

    deploy.sh project name changes

--- Deploy.sh
+++ Deploy.sh
@@ -5,7 +5,7 @@ VER=mta_archives/knpl-dga-web_1.0.0.mtar
 cf login -a https://api.cf.ap11.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin@2022 -o "Kansai Nerolac Paints Ltd_knpl-dev-projects" -s "${SPACE}"
 
 echo "Building Project"
-mbt build -s '/home/user/projects/knpl-influencer-web'
+mbt build -s '/home/user/projects/knpl-dga-web'
 
 if [[ $? -eq 0 ]]; then
     echo "build complete Now deploying"
