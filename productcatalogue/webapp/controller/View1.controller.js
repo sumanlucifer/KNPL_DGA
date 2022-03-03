befd4db3df2b3636ddf9664786d47a955ebc58ae
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-03-03 07:35:41 +0000

    products catalogue

---
+++ productcatalogue/webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("com.knpl.dga.productcatalogue.productcatalogue.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
