7eda7209346f28cd9ef4c90f0ce61d51584802dd
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-03-25 11:48:19 +0000

    feature acccess module

---
+++ featureaccess/webapp/model/cmbxDtype2.js
@@ -0,0 +1,32 @@
+sap.ui.define(
+  ["sap/ui/model/SimpleType", "sap/ui/model/ValidateException"],
+  function (SimpleType, ValidateException) {
+    "use strict";
+
+    return SimpleType.extend(
+      "com.knpl.dga.featureaccess.model.cmbxDtype2",
+      {
+        formatValue: function (oValue) {
+         
+          if (oValue == "") {
+            return oValue;
+          } else if (oValue !== "") {
+            return parseInt(oValue);
+          }
+        },
+        parseValue: function (oValue) {
+          var abc = 0;
+          if (oValue == "") {
+            abc = "";
+          } else if (Number.isInteger(parseInt(oValue))) {
+            abc = parseInt(oValue);
+          }
+          return abc;
+        },
+        validateValue: function (oValue) {
+        
+        },
+      }
+    );
+  }
+);
---
+++ featureaccess/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comknpldga/featureaccess/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
