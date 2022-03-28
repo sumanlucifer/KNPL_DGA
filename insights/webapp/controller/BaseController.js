eb0ee63ffacfc93c78a1b88bafeff38cbfc4ce3b
Akhil Jain <akhil.jain@extentia.com>
2022-03-28 07:12:58 +0000

    insights module

---
+++ insights/webapp/controller/BaseController.js
@@ -0,0 +1,393 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+    "sap/m/library",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+    "../controller/Validator",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], function (Controller, UIComponent, mobileLibrary, History, Fragment, JSONModel, Validator, MessageToast, MessageBox, Filter, FilterOperator) {
+    "use strict";
+
+    // shortcut for sap.m.URLHelper
+    var URLHelper = mobileLibrary.URLHelper;
+
+    return Controller.extend("com.knpl.dga.com.knpl.dga.insights..controller.BaseController", {
+        /**
+         * Convenience method for accessing the router.
+         * @public
+         * @returns {sap.ui.core.routing.Router} the router for this component
+         */
+        getRouter: function () {
+            return UIComponent.getRouterFor(this);
+        },
+
+        /**
+         * Convenience method for getting the view model by name.
+         * @public
+         * @param {string} [sName] the model name
+         * @returns {sap.ui.model.Model} the model instance
+         */
+        getModel: function (sName) {
+            return this.getView().getModel(sName);
+        },
+
+        /**
+         * Convenience method for setting the view model.
+         * @public
+         * @param {sap.ui.model.Model} oModel the model instance
+         * @param {string} sName the model name
+         * @returns {sap.ui.mvc.View} the view instance
+         */
+        setModel: function (oModel, sName) {
+            return this.getView().setModel(oModel, sName);
+        },
+        onNavToHome: function () {
+            var sPreviousHash = History.getInstance().getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                history.go(-1);
+            } else {
+                this.getRouter().navTo("worklist", {}, true);
+            }
+            // var oHistory = History.getInstance();
+            // var sPreviousHash = oHistory.getPreviousHash();
+
+            // if (sPreviousHash !== undefined) {
+            //     window.history.go(-1);
+            // } else {
+            //     var oRouter = this.getOwnerComponent().getRouter();
+            //     oRouter.navTo("worklist", {}, true);
+            // }
+        },
+        _dummyPromise: function (oPayload) {
+            var promise = $.Deferred();
+            promise.resolve(oPayload)
+            return promise;
+        },
+        _AddObjectControlModel: function (mParam1, mParam2) {
+            /*
+             * Author: manik saluja
+             * Date: 02-Dec-2021
+             * Language:  JS
+             * Purpose: used to create omodelcontrol that is binded to the view or used to store static data
+             */
+            var promise = jQuery.Deferred();
+            var oView = this.getView();
+            var oDataControl = {
+                PageBusy: true,
+                Pagetitle: mParam1 === "Add" ? "Add Lead" : "Edit Lead",
+                mode: mParam1,
+                LeadId: mParam2,
+                bindProp: "Leads(" + mParam2 + ")",
+                resourcePath: "com.knpl.dga.com.knpl.dga.insights.",
+                AddFields: {
+                    Pincode: ""
+                }
+            };
+            var oModelControl = new JSONModel(oDataControl)
+            oView.setModel(oModelControl, "oModelControl");
+            promise.resolve()
+            return promise;
+        },
+        _ValidateForm: function () {
+            var oView = this.getView();
+            var oValidate = new Validator();
+            var othat = this;
+            var oForm = oView.byId("FormObjectData");
+            var bFlagValidate = oValidate.validate(oForm, true);
+            if (!bFlagValidate) {
+                othat._showMessageToast("Message3")
+                return false;
+            }
+            return true;
+        },
+        _getViewFragment: function (sFragmentName) {
+            /*
+             * Author: manik saluja
+             * Date: 14-March-2022
+             * Language:  JS
+             * Purpose: Common method to access fragmets from folder view.fragments. this method is 
+             * written so that the developer dont writes the Fragment.load again.
+             */
+            var oView = this.getView();
+            var oModel;
+            if (oView.getModel("oModelControl")) {
+                oModel = oView.getModel("oModelControl");
+            } else {
+                oModel = oView.getModel("oModelDisplay");
+            }
+            var othat = this;
+
+            this._formFragments = Fragment.load({
+                id: oView.getId(),
+                name: oModel.getProperty("/resourcePath") + ".view.fragments." + sFragmentName,
+                controller: othat,
+            }).then(function (oFragament) {
+                return oFragament;
+            });
+
+
+            return this._formFragments;
+        },
+        /**
+         * Getter for the resource bundle.
+         * @public
+         * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+         */
+        _geti18nText: function (mParam, mParam2) {
+            var oModel = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            return oModel.getText(mParam, mParam2);
+        },
+        _showMessageToast: function (mParam, mParam2) {
+            var oModel = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            var sText = oModel.getText(mParam, mParam2);
+            MessageToast.show(sText, {
+                duration: 6000
+            })
+        },
+        _showMessageBox1: function (pType, pMessage, pMessageParam, pfn1, pfn2) {
+            // 
+            /*pType(string) > type of message box ex: information or alert etc.
+              pMessage (string)> i18n property name for the message
+              pMessageParam(array/null)> i18n property has params specify in array or else pass as null
+              pfn1(function1/null) > this is a function to be called after user presses yes 
+              pfn2(function2/null) > this is a function to be called after user presses no
+
+              you can call this below method like this
+              this._showMessageBox1("information", "i18nProper", ["i18nParamerter1if any"],
+              this._sample1.bind(this, "first paramters", "secondParameter"));
+              In this code all the message type will have 2 buttons yes and no
+            */
+            var sMessage = this._geti18nText(pMessage, pMessageParam);
+            var sPtye = pType.trim().toLowerCase();
+            var othat = this;
+            var aMessageType = ["success", "information", "alert", "error", "warning", "confirm"];
+
+            if (aMessageType.indexOf(sPtye) >= 0) {
+                MessageBox[sPtye](sMessage, {
+                    actions: [sap.m.MessageBox.Action.NO, sap.m.MessageBox.Action.YES],
+                    emphasizedAction: MessageBox.Action.YES,
+                    onClose: function (sAction) {
+                        if (sAction === "YES") {
+                            if (pfn1) {
+                                pfn1();
+                            }
+                        } else {
+                            if (pfn2) {
+                                pfn2();
+                            };
+                        }
+                    }
+                });
+                return
+            } else {
+                this._showMessageToast("Message6");
+            }
+
+
+        },
+        _showMessageBox2: function (pType, pMessage, pMessageParam, pfn1, pfn2) {
+
+            /*  pType(string) > type of message box ex: information or alert etc.
+                pMessage (string)> i18n property name for the message
+                pMessageParam(array/null)> i18n property has params specify in array or else pass as null
+                pfn1(function1/null) > this is a function to be called after user presses yes 
+                pfn2(function2/null) > this is a function to be called after user presses no
+
+                you can call this below method like this
+                this._showMessageBox1("information", "i18nProper", ["i18nParamerter1if any"],
+                this._sample1.bind(this, "first paramters", "secondParameter"));
+
+                In this code all the message type will have 1 button 
+            */
+            var sMessage = this._geti18nText(pMessage, pMessageParam);
+            var sPtye = pType.trim().toLowerCase();
+            var othat = this;
+            var aMessageType = ["success", "information", "alert", "error", "warning"];
+
+
+            if (aMessageType.indexOf(sPtye) >= 0) {
+                MessageBox[sPtye](sMessage, {
+
+                    onClose: function (sAction) {
+                        // in case for error dialog we will have a close button insttead of okay
+                        if (sAction === "OK" || sAction === "CLOSE") {
+                            if (pfn1) {
+                                pfn1();
+                            }
+                        } else {
+                            if (pfn2) {
+                                pfn2();
+                            };
+                        }
+                    }
+                });
+                return
+            } else {
+                this._showMessageToast("Message6");
+            }
+
+
+        },
+        _RemoveEmptyValue: function (mParam) {
+            var obj = Object.assign({}, mParam);
+            // remove string values
+            for (var b in obj) {
+                if (obj[b] === "") {
+                    obj[b] = null;
+                }
+            }
+            return obj;
+        },
+        _CheckEmptyFieldsPostPayload: function () {
+            var promise = jQuery.Deferred();
+            var oView = this.getView();
+            var oModel = oView.getModel("oModelView");
+            var oModelData = oModel.getData();
+            //1.Clone the payload and convert string to integer values based on odata model entity
+            var oPayLoad = this._RemoveEmptyValue(oModelData);
+            var inTegerProperty = [
+                "ComplaintTypeId",
+            ];
+            for (var y of inTegerProperty) {
+                if (oPayLoad.hasOwnProperty(y)) {
+                    if (oPayLoad[y] !== null) {
+                        oPayLoad[y] = parseInt(oPayLoad[y]);
+                    }
+                }
+            }
+            promise.resolve(oPayLoad);
+            return promise;
+        },
+        _uploadFile: function (oPayLoad) {
+            var promise = jQuery.Deferred();
+            promise.resolve(oPayLoad);
+            return promise;
+        },
+
+        /**
+         * Event handler when the share by E-Mail button has been clicked
+         * @public
+         */
+        onShareEmailPress: function () {
+            var oViewModel = (this.getModel("objectView") || this.getModel("worklistView"));
+            URLHelper.triggerEmail(
+                null,
+                oViewModel.getProperty("/shareSendEmailSubject"),
+                oViewModel.getProperty("/shareSendEmailMessage")
+            );
+        },
+        onDialogClose: function () {
+            /*
+                Internal method to handle the closure of all the dialogs
+                if dialog 1 is open first and on top over that dialog 2 is open
+                then dialog 2 code for closure should be written before dialog 1
+            */
+            if (this._pValueHelpDialog) {
+                this._pValueHelpDialog.destroy();
+                delete this._pValueHelpDialog;
+                return;
+            }
+
+            if (this._ViewImageDialog) {
+                if (this._ViewImageDialog.isOpen()) {
+                    this._ViewImageDialog.close();
+                    return;
+                }
+            }
+        },
+        _handlePinCodeValueHelp: function () {
+            /*
+            * Author: manik saluja
+            * Date: 15-Mar-2022
+            * Language:  JS
+            * Purpose:  Used to handle the pin code pop over in the add dga and edit dga.
+            */
+            var oView = this.getView();
+            if (!this._PinCodeValueHelp) {
+                this._getViewFragment("PincodeValueHelp").then(function (oControl) {
+                    this._PinCodeValueHelp = oControl;
+                    oView.addDependent(this._PinCodeValueHelp);
+                    this._PinCodeValueHelp.open();
+                }.bind(this))
+            }
+        },
+        _onDialogClose: function () {
+            /*
+            * Author: manik saluja
+            * Date: 15-Mar-2022
+            * Language:  JS
+            * Purpose:  Internal method to handle the closure of all the dialogs
+               if dialog 1 is open first and on top over that dialog 2 is open
+               then dialog 2 code for closure should be written before dialog 1
+            */
+            if (this._PinCodeValueHelp) {
+                this._PinCodeValueHelp.destroy();
+                delete this._PinCodeValueHelp;
+                return;
+            }
+        },
+        _handlePinCodeValueHelpConfirm: function (oEvent) {
+            var oSelectedItem = oEvent.getParameter("selectedItem");
+            var oViewModel = this.getView().getModel("oModelView"),
+                oModelControl = this.getView().getModel("oModelControl");
+            var obj = oSelectedItem.getBindingContext().getObject();
+            oModelControl.setProperty(
+                "/AddFields/Pincode",
+                obj["Name"]
+            );
+            oViewModel.setProperty(
+                "/Pincode",
+                obj["Name"]
+            );
+            this._onDialogClose();
+        },
+        _handlePValueHelpSearch: function (oEvent) {
+            /*
+            * Author: manik saluja
+            * Date: 15-Mar-2022
+            * Language:  JS
+            * Purpose: A common method of controllers handle the search for the popovers
+            */
+            var sValue = oEvent.getParameter("value").trim();
+            var sPath = oEvent.getParameter("itemsBinding").getPath();
+            // Pincodes Valuehelp
+            if (sPath === "/MasterPincodes") {
+                if (sValue.length > 0) {
+                    var aFilter = new Filter({
+                        path: "Name",
+                        operator: "Contains",
+                        value1: sValue,
+                        caseSensitive: false,
+                    });
+                } else {
+                    var aFilter = [];
+                }
+                this._PinCodeValueHelp
+                    .getBinding("items")
+                    .filter(aFilter, "Application");
+                return;
+            }
+        },
+        _getDepot: function (sDepotId) {
+            if (!sDepotId) return;
+
+            var sPath = this.getModel().createKey("/MasterDepotSet", {
+                Id: sDepotId
+            }),
+                oModel = this.getModel("oModelControl");
+
+            this.getModel().read(sPath, {
+                success: ele => oModel.setProperty("/AddFields/PainterDepot", ele.Depot)
+            })
+
+        },
+
+    });
+
+});
---
+++ insights/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/knpl/dga/insights/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
