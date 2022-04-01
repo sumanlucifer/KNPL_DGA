a9497bf21e6661124e327fe842acb53dbb966d91
Deepanjali Kumari <deepanjali.kumari@extentia.com>
2022-04-01 12:06:44 +0000

    i18n changes, cloumn width changes

--- reassigcontractors/webapp/controller/Worklist.controller.js
+++ reassigcontractors/webapp/controller/Worklist.controller.js
@@ -281,10 +281,8 @@ sap.ui.define(
                 var iId = oEve.getSource().getBindingContext().getObject().ID,
                     sButton = oEve.getSource().getTooltip().trim().toLowerCase(),
                     sStatus = sButton === "accepted" ? "2" : "rejected" ? "3": "1",
-                    sAcceptedMsg = this._geti18nText("AcceptMsgConfirm"),
-                    sRejectedMsg = this._geti18nText("RejectedMsgConfirm"),
-                    sMessage = sButton === "accepted" ? sAcceptedMsg : "rejected" ? sRejectedMsg: 1;
-                this._showMessageBox("information", sMessage, "", this.onApproveRejectServiceCall.bind(this, iId, sStatus));
+                    sMessage = sButton === "accepted" ? "Approve" : "Reject";
+                this._showMessageBox("information", "MsgConfirm", [sMessage], this.onApproveRejectServiceCall.bind(this, iId, sStatus));
             },
             onApproveRejectServiceCall: function (iId, sStatus) {
                 var oPayLoad = {
