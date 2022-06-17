e4ef988aeaea8c1b96c35827a80dd4d0cc33f45a
Akhil Jain <akhil.jain@extentia.com>
2022-06-17 06:20:49 +0000

    deploy.sh update

--- Deploy.sh
+++ Deploy.sh
@@ -2,7 +2,7 @@ SPACE="DGA"
 VER=mta_archives/knpl-dga-web_0.0.1.mtar
 
 #Login to CF space
-cf login -a https://api.cf.ap11.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin2022 -o "Kansai Nerolac Paints Ltd_knpl-dev-projects" -s "${SPACE}"
+cf login -a https://api.cf.ap11.hana.ondemand.com -u ankit.pundhir@extentia.com -p Admin2022 -o "Kansai Nerolac Paints Ltd_KNPL-QAS-PROJECTS" -s "${SPACE}"
 
 echo "Building Project"
 mbt build -s '/home/user/projects/knpl-dga-web'
